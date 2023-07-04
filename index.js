24d6351d50c20db65e916ff21eb4f182d874d3be
Gregorio <Greg.del801@gmail.com>
2023-07-03 18:36:39 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+const record = [
+  {
+    year: "2015",
+    result: "W"
+  },
+  {
+    year: "2014",
+    result: "N/A"
+  },
+  {
+    year: "2013",
+    result: "L"
+  },
+
+]
+
+//returns winning year if they have won any year
+function superbowlWin(array) {
+  
+  let findWin = array.find(win => win.result === 'W')
+  console.log(findWin)
+
+  if(typeof findWin === 'object'){
+  findWin = findWin.year
+  }
+  
+ return findWin
+}
+
